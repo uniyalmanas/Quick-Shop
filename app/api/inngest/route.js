bb24e549b8df474bcbeb394cbeeb5b21b865b526
Manas Uniyal <uniyalmanas@gmail.com>
2025-07-17 19:16:23 +0530

    Fix import paths for Vercel deployment

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserUpdation } from "@/config/inngest";
-import { syncUserCreation, syncUserDeletion } from "@/functions/inngest"; // Assuming the functions are not in config
+import { inngest } from "../../../functions/inngest";
+import { syncUserCreation, syncUserDeletion } from "../../../functions/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
