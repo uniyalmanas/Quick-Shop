4cbb82f1daf40f8e159b1d44719b8feca0880ff4
Manas Uniyal <uniyalmanas@gmail.com>
2025-07-18 09:30:18 +0530

    favicon aded

---
+++ app/head.js
@@ -0,0 +1,10 @@
+// This file customizes the <head> for the root layout in Next.js App Router
+export default function Head() {
+  return (
+    <>
+      <title>QuickCart - GreatStack</title>
+      <meta name="description" content="E-Commerce with Next.js " />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
